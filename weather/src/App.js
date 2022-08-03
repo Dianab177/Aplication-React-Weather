cb53971894f7b030e30186ffab2e5ba2cc0469f9
DianaB177 <anaidb77@gmail.com>
2022-08-03 18:38:35 +0200

    css update

--- weather/src/App.js
+++ weather/src/App.js
@@ -21,6 +21,11 @@ export default function App() {
           <Forecast />
         </div>
       </div>
+      <div class="container">
+         <small class="weather-link">
+         <a href="https://github.com/Dianab177/React-Weather-App.git" target="_blank" title="link to github"  rel="noopener noreferrer">Open-source code</a>, by <a href="https://www.linkedin.com/in/dianaborrajo/">Diana Borrajo</a> 
+         </small>
+         </div>
     </div>
   );
 }
