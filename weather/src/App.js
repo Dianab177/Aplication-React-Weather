f839c549050ce7a89289c692a3a9eaa4b80abacd
DianaB177 <anaidb77@gmail.com>
2022-08-03 18:40:20 +0200

    corrrect dir link

--- weather/src/App.js
+++ weather/src/App.js
@@ -23,7 +23,7 @@ export default function App() {
       </div>
       <div class="container">
          <small class="weather-link">
-         <a href="https://github.com/Dianab177/React-Weather-App.git" target="_blank" title="link to github"  rel="noopener noreferrer">Open-source code</a>, by <a href="https://www.linkedin.com/in/dianaborrajo/">Diana Borrajo</a> 
+         <a href="https://github.com/Dianab177/Aplication-React-Weather.git" target="_blank" title="link to github"  rel="noopener noreferrer">Open-source code</a>, by <a href="https://www.linkedin.com/in/dianaborrajo/">Diana Borrajo</a> 
          </small>
          </div>
     </div>
